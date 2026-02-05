602668bb2c29c7bbe8e50e36a9de564210a1215d
masegovi96 <mario.segovia.gtz@outlook.com>
2026-02-05 07:28:58 -0600

    Retroalimentación tarea 2

--- Tarea 2 PHP/Parte4.php
+++ Tarea 2 PHP/Parte4.php
@@ -1,6 +1,7 @@
 Haz un programa que declare una cadena y muestre cuántas veces aparece cada letra.
 <?php
 $cadena = "Hola mundo";
+$cadena = str_replace(" ", "", strtolower($cadena));
 $letras = count_chars($cadena, 1);
 foreach ($letras as $ascii => $cantidad) {
     echo chr($ascii) . " Aparece " . $cantidad . " veces.\n";
--- Tarea 2 PHP/Parte6.php
+++ Tarea 2 PHP/Parte6.php
@@ -1,6 +1,18 @@
 Haz un programa que declare una cadena y cuente cuántas vocales tiene.
 <?php
 $cadena = "Esta es la cadena que se muestra.";
-$vocales = preg_match_all('/[aeiouAEIOU]/', $cadena, $coincidencias);
+$cadena_limpia = str_replace(' ', '', $cadena);
+$cadena_limpia = strtolower($cadena_limpia);
+// $vocales = preg_match_all('/[aeiouAEIOU]/', $cadena, $coincidencias);
+$vocales = 0;
+$arreglo_vocales = ['a', 'e', 'i', 'o', 'u'];
+
+// str_split convierte la cadena en un arreglo de caracteres 
+// arreglo = ["e", "s", "t", "a", "e", "s", "l", "a", ...]
+foreach (str_split($cadena_limpia) as $letra){
+    if (in_array($letra, $arreglo_vocales)){
+        $vocales++;
+    }
+}
 echo "La cadena tiene " . $vocales . " vocales.";
 ?>
